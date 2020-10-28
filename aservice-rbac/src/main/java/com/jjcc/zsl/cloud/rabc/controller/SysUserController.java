81bb7d58de72cd0e6084181f209fb1651bf30330
jjcc123312 <47164229+jjcc123312@users.noreply.github.com>
2020-10-28 17:30:56 +0800

    nacos统一配置信息

--- aservice-rbac/src/main/java/com/jjcc/zsl/cloud/rabc/controller/SysUserController.java
+++ aservice-rbac/src/main/java/com/jjcc/zsl/cloud/rabc/controller/SysUserController.java
@@ -1,6 +1,8 @@
 package com.jjcc.zsl.cloud.rabc.controller;
 
+import com.jjcc.zsl.cloud.rabc.domain.OrderProperties;
 import com.jjcc.zsl.cloud.rabc.feign.SmsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +20,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class SysUserController {
 
+    @Value(value = "${order.pay-timeout-seconds}") // @NacosValue(value = "${order.pay-timeout-seconds}")
+    private Integer payTimeoutSeconds;
+    @Value(value = "${order.create-frequency-seconds}") // @NacosValue(value = "${order.create-frequency-seconds}")
+    private Integer createFrequencySeconds;
+
+
     private final SmsService smsService;
 
-    public SysUserController(SmsService smsService) {
+    private final OrderProperties orderProperties;
+
+    public SysUserController(SmsService smsService, OrderProperties orderProperties) {
         this.smsService = smsService;
+        this.orderProperties = orderProperties;
     }
+
+
 
     @GetMapping(value = "/pwd/reset")
     public ResponseEntity<Object> pwdreset(@RequestParam Integer userId) {
 //        sysuserService.pwdreset(userId);
-        String val = smsService.send("123", "更换密码");
-        return new ResponseEntity<>(val, HttpStatus.OK);
+//        String val = smsService.send("123", "更换密码");
+        String a = "payTimeoutSeconds: " + payTimeoutSeconds + "；createFrequencySeconds："
+                + createFrequencySeconds;
+        return new ResponseEntity<>(a, HttpStatus.OK);
     }
 }
---
+++ aservice-rbac/src/main/java/com/jjcc/zsl/cloud/rabc/domain/OrderProperties.java
@@ -0,0 +1,25 @@
+package com.jjcc.zsl.cloud.rabc.domain;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "order")
+// @NacosConfigurationProperties(prefix = "order", dataId = "${nacos.config.data-id}", type = ConfigType.YAML)
+public class OrderProperties {
+
+    /**
+     * 订单支付超时时长，单位：秒。
+     */
+    private Integer payTimeoutSeconds;
+
+    /**
+     * 订单创建频率，单位：秒
+     */
+    private Integer createFrequencySeconds;
+
+    // ... 省略 setter/getter 方法
+
+}
