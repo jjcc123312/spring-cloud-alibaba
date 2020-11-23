6902f2f3f081d8e55d4adb744fd227a24fbbd87b
jjcc123312 <47164229+jjcc123312@users.noreply.github.com>
2020-11-23 14:46:02 +0800

    hystrix-dashboard单服务监控

---
+++ service-hystrix-dashboard/src/main/java/com/jjcc/zsl/cloud/servicehystrixdashboard/ServiceHystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.jjcc.zsl.cloud.servicehystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class ServiceHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHystrixDashboardApplication.class, args);
+    }
+
+}
